df1dc7a79f85d726d39e3dddc4fe3d5d7747e257
Felipepereiralima <felipeplima@live.com>
2021-12-06 22:54:22 -0300

    Atualização do projeto com model e service

--- Blog-Pessoal/src/app/entrar/entrar.component.ts
+++ Blog-Pessoal/src/app/entrar/entrar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
+import { UserLogin } from '../model/UserLogin';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-entrar',
@@ -7,9 +11,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EntrarComponent implements OnInit {
 
-  constructor() { }
+  UserLogin: UserLogin = new UserLogin()
 
-  ngOnInit(): void {
+  constructor(
+    private authService: AuthService,
+    private router: Router
+
+  ) { }
+
+  ngOnInit(){
+    window.scroll(0,0)
+
+  }
+  entrar(){
+    this.authService.entrar(this.UserLogin).subscribe((resp: UserLogin)=>{
+      this.UserLogin = resp
+
+      /* Variáveis globais */
+      environment.token = this.UserLogin.token
+      environment.nome = this.UserLogin.nome
+      environment.foto = this.UserLogin.foto
+      environment.id = this.UserLogin.id
+
+      /* Ver as informações no console
+      console.log(environment.token)
+      console.log(environment.nome)
+      console.log(environment.foto)
+      console.log(environment.id)*/
+
+      this.router.navigate(['/inicio'])
+    }, erro =>{
+      if(erro.status == 401){
+        alert("Usuário ou senha estão incorretos.")
+      }
+    })
   }
 
 }
